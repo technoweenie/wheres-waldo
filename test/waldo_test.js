04b16c49d7e80e115c27c398bb7e370effec87a7
rick <technoweenie@gmail.com>
2010-01-08 17:54:06 -0800

    get tests passing with yucky blocking methods

--- test/waldo_test.js
+++ test/waldo_test.js
@@ -1,13 +1,14 @@
 var redisclient = require('redisclient'),
     whereswaldo = require('../lib'),
-         assert = require('assert');
+         assert = require('assert'),
+            sys = require('sys'),
+          redis = new redisclient.Client();
 
 process.mixin(GLOBAL, require('ntest'))
 
 describe("new waldo instance")
   before(function() {
-    this.redis = new redisclient.Client()
-    this.waldo = whereswaldo.create(this.redis);
+    this.waldo = whereswaldo.create(redis);
   })
 
   it("has a default prefix", function() {
@@ -15,13 +16,28 @@ describe("new waldo instance")
   })
 
   it('sets redis instance', function() {
-    assert.equal(this.redis, this.waldo.redis)
+    assert.equal(redis, this.waldo.redis)
   })
 
   it('does not know where a user is', function() {
-    assert.ok(!this.waldo.locate('bob'))
+    assert.ok(!this.waldo.locate('fred'))
   })
 
   it('returns no users for an empty location', function() {
-    assert.equal([], this.waldo.list('home'))
+    var users = this.waldo.list('home');
+    assert.equal(0, users.length)
+  })
+
+describe("tracking a user")
+  before(function() {
+    this.waldo = whereswaldo.create(redis);
+    this.waldo.track('bob', 'gym')
+  })
+
+  it("tracks a user's location", function() {
+    assert.equal('gym', this.waldo.locate('bob'))
+  })
+
+  it("lists the user in that location", function() {
+    assert.equal('bob', this.waldo.list('gym')[0])
   })
