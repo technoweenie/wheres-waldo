986c96e643abfbd73fd69aa3602cc483c10813e2
rick <technoweenie@gmail.com>
2010-01-31 18:29:53 -0800

    use fab for serving api requests

--- README.md
+++ README.md
@@ -4,11 +4,12 @@ Track what users are on which pages with redis.
 
 ## Installation
 
-Depends on ntest, redisclient, and underscore.
+Depends on fab, ntest, redisclient, and underscore.
 
 There's no packaging system for node.js yet, so I've just been creating symlinks
 in my `~/.node_libraries` path.
 
+    $ ln -s /path/to/fab              ~/.node_libraries/fab
     $ ln -s /path/to/redisclient      ~/.node_libraries/redisclient
     $ ln -s /path/to/underscore       ~/.node_libraries/underscore
     $ ln -s /path/to/wheres-waldo/lib ~/.node_libraries/wheres-waldo
--- test/api_test.js
+++ test/api_test.js
@@ -23,55 +23,55 @@ waldoServer.server.addListener('close', function(e) {
 before(function() { redis.flushdb().wait() })
 
 it("cannot locate missing user", function() {
-  var resp = client.request("/locate?name=bob").wait();
+  var resp = client.request("/waldo/locate?name=bob").wait();
   assert.equal('""', resp.body);
 })
 
 it("lists empty location", function() {
-  var resp = client.request("/list?location=gym").wait();
+  var resp = client.request("/waldo/list?location=gym").wait();
   assert.equal('[]', resp.body);
 })
 
 it("tracks user's location", function() {
-  client.request("/track?name=bob&location=gym").wait()
-  var resp = client.request("/locate?name=bob").wait();
+  client.request("/waldo/track?name=bob&location=gym").wait()
+  var resp = client.request("/waldo/locate?name=bob").wait();
   assert.equal('"gym"', resp.body);
 
-  var resp = client.request("/list?location=gym").wait();
+  var resp = client.request("/waldo/list?location=gym").wait();
   assert.equal('["bob"]', resp.body);
 })
 
 it("tracks a user and lists users for that location", function() {
-  client.request("/track?name=thing1&location=hat").wait()
-  var resp = client.request("/track_and_list?name=thing2&location=hat").wait()
+  client.request("/waldo/track?name=thing1&location=hat").wait()
+  var resp = client.request("/waldo/track_and_list?name=thing2&location=hat").wait()
   assert.equal('["thing1","thing2"]', resp.body)
 })
 
 it('handles bad 404 requests', function() {
-  var resp = client.request("/").wait()
+  var resp = client.request("/waldo").wait()
   assert.equal(404, resp.statusCode)
 })
 
 it('ignores invalid locate request', function() {
-  var resp = client.request("/locate").wait();
+  var resp = client.request("/waldo/locate").wait();
   assert.equal('""', resp.body);
 })
 
 it('ignores invalid list request', function() {
-  var resp = client.request("/list").wait();
+  var resp = client.request("/waldo/list").wait();
   assert.equal('""', resp.body);
 })
 
 it('ignores invalid track_and_list request', function() {
-  var resp = client.request("/track_and_list").wait();
+  var resp = client.request("/waldo/track_and_list").wait();
   assert.equal('""', resp.body);
 })
 
 it('ignores invalid track request', function() {
   promise = promises.group(
-    client.request("/track"),
-    client.request("/track?name=1"),
-    client.request("/track?location=1"))
+    client.request("/waldo/track"),
+    client.request("/waldo/track?name=1"),
+    client.request("/waldo/track?location=1"))
   var resps = promise.wait()
   resps.forEach(function(resp) {
     assert.equal('false', resp.body)
@@ -79,33 +79,23 @@ it('ignores invalid track request', function() {
 })
 
 it('wraps locate request in callback', function() {
-  var resp = client.request("/locate?callback=foo").wait();
+  var resp = client.request("/waldo/locate?callback=foo").wait();
   assert.equal('foo("")', resp.body);
 })
 
 it('wraps list request in callback', function() {
-  var resp = client.request("/list?callback=foo").wait();
+  var resp = client.request("/waldo/list?callback=foo").wait();
   assert.equal('foo("")', resp.body);
 })
 
 it('wraps track_and_list request in callback', function() {
-  var resp = client.request("/track_and_list?callback=foo").wait();
+  var resp = client.request("/waldo/track_and_list?callback=foo").wait();
   assert.equal('foo("")', resp.body);
 })
 
 it('wraps track request in callback', function() {
-  var resp = client.request("/track?callback=foo").wait();
+  var resp = client.request("/waldo/track?callback=foo").wait();
   assert.equal('foo(false)', resp.body);
-})
-
-it('emits request event', function() {
-  promise = listeners.wrap(waldoServer, 'request', function(r, s) {
-    assert.equal('/foo', r.url)
-    assert.equal(404, s)
-    promise.emitSuccess()
-  })
-  client.request("/foo")
-  promise.wait()
 })
 
 it('emits track event', function() {
@@ -114,29 +104,29 @@ it('emits track event', function() {
     assert.equal('work', location)
     promise.emitSuccess()
   })
-  client.request("/track?name=mark&location=work")
+  client.request("/waldo/track?name=mark&location=work")
   promise.wait()
 })
 
 it('emits locate event', function() {
-  client.request("/track?name=mark&location=work")
+  client.request("/waldo/track?name=mark&location=work")
   promise = listeners.wrap(waldoServer, 'locate', function(user, location) {
     assert.equal('mark', user)
     assert.equal('work', location)
     promise.emitSuccess()
   })
-  client.request("/locate?name=mark")
+  client.request("/waldo/locate?name=mark")
   promise.wait()
 })
 
 it('emits list event', function() {
-  client.request("/track?name=mark&location=work")
+  client.request("/waldo/track?name=mark&location=work")
   promise = listeners.wrap(waldoServer, 'list', function(location, users) {
     assert.equal('mark', users[0])
     assert.equal('work', location)
     promise.emitSuccess()
   })
-  client.request("/list?location=work")
+  client.request("/waldo/list?location=work")
   promise.wait()
 })
 
